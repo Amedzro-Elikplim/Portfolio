3ec2d83b4824d485d043cbaea9d057a56291946a
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-02-01 16:21:51 +0000

    Update double quotes to single quotes and changed indentation

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
-//reference the mobile navbar icon and mobile menu
-const barIcon = document.querySelector(".bar-icon");
-const mobileMenuDropdown = document.querySelector(".dropdown-menu");
-const lists = document.querySelectorAll(".menu-nav-links > li");
-const body = document.querySelector(".main-body");
+const barIcon = document.querySelector('.bar-icon');
+const mobileMenuDropdown = document.querySelector('.dropdown-menu');
+const lists = document.querySelectorAll('.menu-nav-links > li');
+const body = document.querySelector('.main-body');
 const closeIcon = document.querySelector('.dropdown-menu-icon');
 
-
 barIcon.addEventListener('click', () => {
-    mobileMenuDropdown.style.display = "block";
-    body.style.overflow = "hidden";
+  mobileMenuDropdown.style.display = 'block';
+  body.style.overflow = 'hidden';
 });
 
-
 lists.forEach((items) => {
-    items.addEventListener('click', () => {
-        mobileMenuDropdown.style.display = "none";
-        body.style.overflow = "visible"
-    })
-})
+  items.addEventListener('click', () => {
+    mobileMenuDropdown.style.display = 'none';
+    body.style.overflow = 'visible';
+  });
+});
 
 closeIcon.addEventListener('click', () => {
-    mobileMenuDropdown.style.display = "none";
-})
+  mobileMenuDropdown.style.display = 'none';
+});
