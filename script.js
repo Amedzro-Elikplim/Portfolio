3bfdf7980a0e5b14bba1bb073a103978230953e5
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-02-01 21:29:00 +0000

    Add div to page dynamically

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const mobileMenuDropdown = document.querySelector('.dropdown-menu');
 const lists = document.querySelectorAll('.menu-nav-links > li');
 const body = document.querySelector('.main-body');
 const closeIcon = document.querySelector('.dropdown-menu-icon');
+const button1 = document.querySelector('.project1-button');
+const project1Container = document.querySelector('.project-info-container1');
+
 
 barIcon.addEventListener('click', () => {
   mobileMenuDropdown.style.display = 'block';
@@ -20,3 +23,42 @@ closeIcon.addEventListener('click', () => {
   mobileMenuDropdown.style.display = 'none';
   body.style.overflow = 'visible';
 });
+
+const details = [
+  {
+    project1: {
+      name: 'Scientific Calculator',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis vel a ea sequi quidem culpa accusamus possimus, aliquid delectus, id molestias consequatur',
+      featuredImage: './assets/images/background.jpg',
+      technologies: ['Html/Css', 'Javascript', 'Ruby on rails'],
+      liveVersion: 'https://amedzro-elikplim.github.io/Portfolio/',
+      source: 'https://github.com/Amedzro-Elikplim/Portfolio',
+    },
+  },
+
+  {
+    project2: {
+      name: 'Calculater',
+    },
+  },
+];
+
+
+function createProjects() {
+  
+  const container = document.createElement('div');
+  container.className = 'project-container';
+  container.innerHTML = `<button class="close-button" id="close" onclick=${write()}>exit</button>`;
+  
+function write() {
+  console.log('hello');
+}
+  
+  project1Container.appendChild(container);
+}
+
+
+
+button1.addEventListener('click', createProjects);
+
+
