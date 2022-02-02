ca02899395103461b1a9dfbbdf23ef388b97d94e
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-02-02 17:05:11 +0000

    Update query selector reference for button

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ const mobileMenuDropdown = document.querySelector('.dropdown-menu');
 const lists = document.querySelectorAll('.menu-nav-links > li');
 const body = document.querySelector('.main-body');
 const closeIcon = document.querySelector('.dropdown-menu-icon');
-const button1 = document.querySelector('.project1-button');
-const project1Container = document.querySelector('.project-info-container1');
+
+const projectContainer = document.querySelector('.project-info');
+
+const button = document.querySelectorAll('.project-button');
 
 
 barIcon.addEventListener('click', () => {
@@ -24,46 +26,85 @@ closeIcon.addEventListener('click', () => {
   body.style.overflow = 'visible';
 });
 
+
+class Project {
+  costructor(
+    name,
+    description,
+    featureImage,
+    technologies,
+    liveVersion,
+    source
+  ) {
+    this.name = name;
+    this.description = description;
+    this.featureImage = featureImage;
+    this.technologies = technologies;
+    this.liveVersion = liveVersion;
+    this.source = source;
+  }
+}
+
 const details = [
   {
     project1: {
-      name: 'Scientific Calculator',
+      name: "Scientific Calculator",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis vel a ea sequi quidem culpa accusamus possimus, aliquid delectus, id molestias consequatur",
+      featuredImage: "./assets/images/background.jpg",
+      technologies: ["Html/Css", "Javascript", "Ruby on rails"],
+      liveVersion: "https://amedzro-elikplim.github.io/Portfolio/",
+      source: "https://github.com/Amedzro-Elikplim/Portfolio",
+    },
+
+    project2: {
+      name: 'Scientific',
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis vel a ea sequi quidem culpa accusamus possimus, aliquid delectus, id molestias consequatur',
       featuredImage: './assets/images/background.jpg',
       technologies: ['Html/Css', 'Javascript', 'Ruby on rails'],
       liveVersion: 'https://amedzro-elikplim.github.io/Portfolio/',
       source: 'https://github.com/Amedzro-Elikplim/Portfolio',
     },
+  
   },
 
-  {
-    project2: {
-      name: 'Calculater',
-    },
-  },
+  
 ];
+
+
 
 
 function createProjects() {
   
   const container = document.createElement('div');
   const closeIcon = document.createElement('img');
+
+ 
   closeIcon.src = '/assets/images/Icon.png';
   container.className = 'project-container';
-  container.appendChild(closeIcon);
+
+
   closeIcon.className = 'close-button';
-  button1.classList.toggle('hidden');
+  // button.classList.toggle('hidden');
   container.style.overflow = 'scroll';
+  // container.classList.toggle('active');
+  
 closeIcon.addEventListener('click', ()=>{
     container.classList.toggle('hidden');
-    button1.classList.toggle('active');
+    // button.classList.toggle('active');
 })
   
-  project1Container.appendChild(container);
+  container.appendChild(closeIcon);
+  projectContainer.appendChild(container);
+
+  console.log('please work')
 }
 
 
 
-button1.addEventListener('click', createProjects);
+button.forEach((element) => {
+   element.addEventListener("click", createProjects);
+})
+
 
 
