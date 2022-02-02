3454da0d048c017da964d18c58c9bf86952ec5ef
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-02-02 20:22:10 +0000

    Add elements dynamically to project1 popup window

--- script.js
+++ script.js
@@ -3,10 +3,7 @@ const mobileMenuDropdown = document.querySelector('.dropdown-menu');
 const lists = document.querySelectorAll('.menu-nav-links > li');
 const body = document.querySelector('.main-body');
 const closeIcon = document.querySelector('.dropdown-menu-icon');
-
-const projectContainer = document.querySelector('.project-info');
-
-const button = document.querySelectorAll('.project-button');
+const button1 = document.querySelector('.project-button1');
 
 
 barIcon.addEventListener('click', () => {
@@ -27,23 +24,6 @@ closeIcon.addEventListener('click', () => {
 });
 
 
-class Project {
-  costructor(
-    name,
-    description,
-    featureImage,
-    technologies,
-    liveVersion,
-    source
-  ) {
-    this.name = name;
-    this.description = description;
-    this.featureImage = featureImage;
-    this.technologies = technologies;
-    this.liveVersion = liveVersion;
-    this.source = source;
-  }
-}
 
 const details = [
   {
@@ -55,56 +35,84 @@ const details = [
       technologies: ["Html/Css", "Javascript", "Ruby on rails"],
       liveVersion: "https://amedzro-elikplim.github.io/Portfolio/",
       source: "https://github.com/Amedzro-Elikplim/Portfolio",
+      btn1Name: "See live",
+      btn2Name: "See source",
     },
 
     project2: {
-      name: 'Scientific',
-      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis vel a ea sequi quidem culpa accusamus possimus, aliquid delectus, id molestias consequatur',
-      featuredImage: './assets/images/background.jpg',
-      technologies: ['Html/Css', 'Javascript', 'Ruby on rails'],
-      liveVersion: 'https://amedzro-elikplim.github.io/Portfolio/',
-      source: 'https://github.com/Amedzro-Elikplim/Portfolio',
+      name: "Scientific",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis vel a ea sequi quidem culpa accusamus possimus, aliquid delectus, id molestias consequatur",
+      featuredImage: "./assets/images/background.jpg",
+      technologies: ["Html/Css", "Javascript", "Ruby on rails"],
+      liveVersion: "https://amedzro-elikplim.github.io/Portfolio/",
+      source: "https://github.com/Amedzro-Elikplim/Portfolio",
     },
-  
   },
-
-  
 ];
 
 
 
 
 function createProjects() {
-  
+  const projectContainer = document.querySelector('.project-info-container1');
   const container = document.createElement('div');
   const closeIcon = document.createElement('img');
+  const heading = document.createElement('h1');
+  const projectImage = document.createElement('img');
+  const description = document.createElement('p');
+  const technologies = document.createElement('ul');
+  const list1 = document.createElement('li');
+  const list2 = document.createElement('li');
+  const list3 = document.createElement('li');
+  const link1 = document.createElement('a');
+  const link2 = document.createElement('a');
+  
+  
 
- 
+
+
+  heading.innerHTML = details[0].project1.name;
+  projectImage.src = details[0].project1.featuredImage;
+  description.innerHTML = details[0].project1.description;
   closeIcon.src = '/assets/images/Icon.png';
+  list1.innerHTML = details[0].project1.technologies[0];
+  list2.innerHTML = details[0].project1.technologies[1];
+  list3.innerHTML = details[0].project1.technologies[2];
+  link1.innerHTML = details[0].project1.btn1Name;
+  link1.href = details[0].project1.liveVersion;
+  link2.innerHTML = details[0].project1.btn2Name;
+  link2.href = details[0].project1.source;
+  
   container.className = 'project-container';
-
-
   closeIcon.className = 'close-button';
-  // button.classList.toggle('hidden');
-  container.style.overflow = 'scroll';
-  // container.classList.toggle('active');
+  
   
 closeIcon.addEventListener('click', ()=>{
     container.classList.toggle('hidden');
-    // button.classList.toggle('active');
 })
   
+  technologies.appendChild(list1);
+  technologies.appendChild(list2);
+  technologies.appendChild(list3);
+  
   container.appendChild(closeIcon);
+  container.appendChild(heading);
+  container.appendChild(projectImage);
+  container.appendChild(description);
+  container.appendChild(technologies);
+  container.appendChild(link1);
+  container.appendChild(link2);
+
   projectContainer.appendChild(container);
 
-  console.log('please work')
 }
 
 
 
-button.forEach((element) => {
-   element.addEventListener("click", createProjects);
-})
+button1.addEventListener("click", createProjects);
+
+
 
 
 
