c1a2df977bcd4bff3bf0a56e54fcdc462f957968
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-02-05 00:00:30 +0000

    Add icon to project button

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ const lists = document.querySelectorAll('.menu-nav-links > li');
 const body = document.querySelector('.main-body');
 const closeIcon = document.querySelector('.dropdown-menu-icon');
 
-const desktopProjectContainer = document.querySelector('.project');
-
 barIcon.addEventListener('click', () => {
   mobileMenuDropdown.style.display = 'block';
   body.style.overflow = 'hidden';
@@ -37,50 +35,6 @@ const details = [
   },
 ];
 
-function projectTemplate(className, infoContainer) {
-  const project = `
-    <div class=${className}>
-        <div class="project-image-container">
-          <img
-            src="./assets/images/art.png"
-            alt="project image"
-            class="project-image"
-          />
-        </div>
-
-        <div>
-          <div class=${infoContainer}>
-            <h2 class="project-name">Multi-Post Stories</h2>
-            <p class="project-description">
-              A daily selection of privately personalized reads; no accounts or
-              sign-ups required. has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a standard
-              dummy text.
-            </p>
-          </div>
-            <button onclick="popup('.project-info-container1')" class="project-button project-button1"> </button>
-          <div class="technologies">
-            <ul>
-              <li>css</li>
-              <li>html</li>
-              <li>bootstrap</li>
-              <li>ruby</li>
-            </ul>
-          </div>
-
-          
-        </div>
-      </div>
-
-  `;
-
-  return project;
-}
-
-// function dosomething() {
-//   console.log("hello");
-// }
-
 const workSection = document.querySelector('.work-section');
 const arrClassName = ['project', 'project-reverse project', 'project', 'project-reverse1 project'];
 const arrInfo = [
@@ -90,18 +44,12 @@ const arrInfo = [
   'project-info-container4',
 ];
 
-const arrButton = [
-  'project-button project-button1',
-  'project-button project-button2',
-  'project-button project-button3',
-  'project-button project-button4',
+const popInfo = [
+  'project-info-container1',
+  'project-info-container2',
+  'project-info-container3',
+  'project-info-container4',
 ];
-
-const buttonID = ['button1', 'button2', 'button3', 'button4'];
-
-for (let i = 0; i < 4; i += 1) {
-  workSection.innerHTML += projectTemplate(arrClassName[i], arrInfo[i]);
-}
 
 function heading() {
   const heading = document.createElement('h1');
@@ -131,7 +79,11 @@ function projectImage() {
 
 function liveLink() {
   const link1 = document.createElement('a');
-  link1.innerHTML = details[0].btn1Name;
+  const div1 = document.createElement('div');
+  const div2 = document.createElement('div');
+  link1.append(div1, div2);
+  div1.innerHTML = details[0].btn1Name;
+  div2.innerHTML = '<i class="fas fa-external-link-alt"></i>';
   link1.href = details[0].liveVersion;
   link1.className = 'links';
 
@@ -140,14 +92,19 @@ function liveLink() {
 
 function sourceLink() {
   const link2 = document.createElement('a');
-  link2.innerHTML = details[0].btn2Name;
+  const div1 = document.createElement('div');
+  const div2 = document.createElement('div');
+  div1.innerHTML = details[0].btn2Name;
+  div2.innerHTML = '<i class="fab fa-github"></i>';
   link2.href = details[0].source;
+  link2.append(div1, div2);
   link2.className = 'links';
 
   return link2;
 }
 
 function popup(name) {
+  const desktopProjectContainer = document.querySelector('.project');
   const projectContainer = document.querySelector(name);
   const backgroundDiv = document.createElement('div');
   const container = document.createElement('div');
@@ -189,6 +146,53 @@ function popup(name) {
   const attach = window.innerWidth < 567 ? projectContainer : desktopProjectContainer;
   attach.appendChild(backgroundDiv);
 }
-// button2.addEventListener('click', () => popup('.project-info-container2'));
-// button3.addEventListener('click', () => popup('.project-info-container3'));
-// button4.addEventListener('click', () => popup('.project-info-container4'));
+
+function projectTemplate(className, infoContainer, popInfo) {
+  const project = `
+    <div class=${className}>
+        <div class="project-image-container">
+          <img
+            src="./assets/images/art.png"
+            alt="project image"
+            class="project-image"
+          />
+        </div>
+
+        <div>
+          <div class=${infoContainer}>
+            <h2 class="project-name">Multi-Post Stories</h2>
+            <p class="project-description">
+              A daily selection of privately personalized reads; no accounts or
+              sign-ups required. has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a standard
+              dummy text.
+            </p>
+          </div>
+            <button onclick="popup('.${popInfo}')" class="project-button project-button1">See project</button>
+          <div class="technologies">
+            <ul>
+              <li>css</li>
+              <li>html</li>
+              <li>bootstrap</li>
+              <li>ruby</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+  `;
+
+  return project;
+}
+
+for (let i = 0; i < 4; i += 1) {
+  workSection.innerHTML += projectTemplate(
+    arrClassName[i],
+    arrInfo[i],
+    popInfo[i],
+  );
+}
+
+const mobile = 'small';
+if (mobile === 'large') {
+  popup();
+}
