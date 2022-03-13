b94a26e9d489d0c9e3ba3a06caf5209875f88ace
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-03-13 16:52:27 +0000

    Refactor code to dynamically display project template

--- script.js
+++ script.js
@@ -84,39 +84,39 @@ const details = [
   {
     name: 'Minimalist',
     description: 'A simple Todo app to add list of books a user has read. It is implemented using HTML, CSS, Webpack, JavaScript and modular architecture. A todo is saved and displayed in Realtime. A todo can be removed from the list of to-dos. Data persistence is implemented using local Storage API.',
-    featuredImage: './assets/images/minimalist.JPG',
-    featuredImage2: './assets/images/desktop-image.svg',
-    technology: ['Html/Css', 'Javascript', 'Ruby on rails'],
-    liveVersion: 'https://amedzro-elikplim.github.io/Portfolio/',
-    source: 'https://github.com/Amedzro-Elikplim/Portfolio',
+    featuredImage: './assets/images/minimalist-desktop.JPG',
+    featuredImage2: './assets/images/minimalist-mobile.JPG',
+    technology: ['Html', 'Css', 'Javascript', 'Webpack'],
+    liveVersion: 'https://amedzro-elikplim.github.io/Minimalist/dist/',
+    source: 'https://github.com/Amedzro-Elikplim/Minimalist',
     btn1Name: 'See live',
     btn2Name: 'See source',
   },
   {
-    name: 'Multi - Post Stories',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis vel a ea sequi quidem culpa accusamus possimus, aliquid d nting and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since theelectus, id molestias consequatur',
-    featuredImage: './assets/images/Snapshoot.svg',
-    featuredImage2: './assets/images/desktop-image.svg',
-    technology: ['Html/Css', 'Javascript', 'Ruby on rails'],
-    liveVersion: 'https://amedzro-elikplim.github.io/Portfolio/',
-    source: 'https://github.com/Amedzro-Elikplim/Portfolio',
+    name: 'Terappy',
+    description: 'Terappy is a platform where individuals with mental health issues can easily find therapist and other mental health professional. The user can find and book appointments with therapists, and also choose the medium of communication (audio / video). Weekly progress of user is monitored',
+    featuredImage: './assets/images/terappy-desktop.JPG',
+    featuredImage2: './assets/images/terappy-mobile.JPG',
+    technology: ['React', 'Netlify', 'MongoDB', 'Node'],
+    liveVersion: 'https://terappy.netlify.app/',
+    source: 'https://github.com/Amedzro-Elikplim/Terappy-frontend/',
     btn1Name: 'See live',
     btn2Name: 'See source',
   },
   {
-    name: 'Multi - Post Stories',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis vel a ea sequi quidem culpa accusamus possimus, aliquid d nting and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since theelectus, id molestias consequatur',
-    featuredImage: './assets/images/Snapshoot.svg',
-    featuredImage2: './assets/images/desktop-image.svg',
-    technology: ['Html/Css', 'Javascript', 'Ruby on rails'],
-    liveVersion: 'https://amedzro-elikplim.github.io/Portfolio/',
-    source: 'https://github.com/Amedzro-Elikplim/Portfolio',
+    name: 'Stock tracker',
+    description: 'Stocker tracker is built as part of Meta and Andela initiative to train developers across africa.It is software used to keep track of stocks in the health sector(clinic, hospital) etc.The frontend is built using react and CORE UI template and hosted on netlify.The backend is built with node, graphql and hosted on heroku',
+    featuredImage: './assets/images/tracker-desktop.JPG',
+    featuredImage2: './assets/images/tracker-mobile.JPG',
+    technology: ['React', 'Nodejs', 'Graphql', 'MongoDB'],
+    liveVersion: 'https://stock-tracker01.netlify.app/#/',
+    source: 'https://github.com/BuildForSDGCohort2/Team-235-Frontend',
     btn1Name: 'See live',
     btn2Name: 'See source',
   },
   {
-    name: 'Multi - Post Stories',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis vel a ea sequi quidem culpa accusamus possimus, aliquid d nting and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since theelectus, id molestias consequatur',
+    name: 'Hacfiery Developers Summit',
+    description: 'This is a platform dedicated to Hackfiery Developers Global Summit 2022. It showcase the speakers, previous conferences and their respective locations',
     featuredImage: './assets/images/Snapshoot.svg',
     featuredImage2: './assets/images/desktop-image.svg',
     technology: ['Html/Css', 'Javascript', 'Ruby on rails'],
@@ -143,58 +143,58 @@ const popInfo = [
   'project-info-container4',
 ];
 
-function heading() {
+function heading(name) {
   const heading = document.createElement('h1');
-  heading.innerHTML = details[0].name;
+  heading.innerHTML = name;
   heading.className = 'heading';
 
   return heading;
 }
 
-function description() {
+function description(descrip) {
   const description = document.createElement('p');
-  description.innerHTML = details[0].description;
+  description.innerHTML = descrip;
   description.className = 'description';
 
   return description;
 }
 
-function projectImage() {
+function projectImage(image, image2) {
   const projectImage = document.createElement('img');
-  const source = (window.innerWidth < 700) ? details[0].featuredImage : details[0].featuredImage2;
+  const source = (window.innerWidth < 700) ? image2 : image;
   projectImage.src = source;
   projectImage.className = 'projectImage';
 
   return projectImage;
 }
 
-function liveLink() {
+function liveLink(liveVersion) {
   const link1 = document.createElement('a');
   const div1 = document.createElement('div');
   const div2 = document.createElement('div');
   link1.append(div1, div2);
   div1.innerHTML = details[0].btn1Name;
   div2.innerHTML = '<i class="fas fa-external-link-alt"></i>';
-  link1.href = details[0].liveVersion;
+  link1.href = liveVersion;
   link1.className = 'links';
 
   return link1;
 }
 
-function sourceLink() {
+function sourceLink(source) {
   const link2 = document.createElement('a');
   const div1 = document.createElement('div');
   const div2 = document.createElement('div');
   div1.innerHTML = details[0].btn2Name;
   div2.innerHTML = '<i class="fab fa-github"></i>';
-  link2.href = details[0].source;
+  link2.href = source;
   link2.append(div1, div2);
   link2.className = 'links';
 
   return link2;
 }
 
-function popup(name) {
+function popup(name, descrip, image, image2, projectName, liveVersion, source) {
   const desktopProjectContainer = document.querySelector('.project');
   const projectContainer = document.querySelector(name);
   const backgroundDiv = document.createElement('div');
@@ -213,9 +213,9 @@ function popup(name) {
   container.classList.add('visible');
   backgroundDiv.classList.add('scroll');
 
-  heading();
-  description();
-  projectImage();
+  heading(projectName);
+  description(descrip);
+  projectImage(image, image2);
 
   const arr = details[0].technology;
   for (let i = 0; i < arr.length; i += 1) {
@@ -230,15 +230,19 @@ function popup(name) {
     backgroundDiv.classList.toggle('hidden');
   });
 
-  buttonWrapper.append(liveLink(), sourceLink());
-  container.append(closeIcon, heading(), projectImage(), description(), technology, buttonWrapper);
+  buttonWrapper.append(liveLink(liveVersion), sourceLink(source));
+  container
+    .append(closeIcon,
+      heading(projectName),
+      projectImage(image, image2), description(descrip), technology, buttonWrapper);
   backgroundDiv.appendChild(container);
 
   const attach = window.innerWidth < 567 ? projectContainer : desktopProjectContainer;
   attach.appendChild(backgroundDiv);
 }
 
-function projectTemplate(className, infoContainer, popInfo, description, name, image) {
+function projectTemplate(className, infoContainer, popInfo,
+  description, name, image, image2, liveVersion, source, tech1, tech2, tech3, tech4) {
   const project = `
     <div class=${className}>
         <div class="project-image-container">
@@ -256,13 +260,13 @@ function projectTemplate(className, infoContainer, popInfo, description, name, i
               ${description}
             </p>
           </div>
-            <button onclick="popup('.${popInfo} ')" class="project-button project-button1">See project</button>
+            <button onclick="popup('.${popInfo}', '${description}', '${image}', '${image2}', '${name}', '${liveVersion}', '${source}')" class="project-button project-button1">See project</button>
           <div class="technologies">
             <ul>
-              <li>css</li>
-              <li>html</li>
-              <li>Javascript</li>
-              <li>ruby</li>
+              <li>${tech1}</li>
+              <li>${tech2}</li>
+              <li>${tech3}</li>
+              <li>${tech4}</li>
             </ul>
           </div>
         </div>
@@ -280,6 +284,13 @@ for (let i = 0; i < details.length; i += 1) {
     details[i].description,
     details[i].name,
     details[i].featuredImage,
+    details[i].featuredImage2,
+    details[i].liveVersion,
+    details[i].source,
+    details[i].technology[0],
+    details[i].technology[1],
+    details[i].technology[2],
+    details[i].technology[3],
   );
 }
 
